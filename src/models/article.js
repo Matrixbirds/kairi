e7edd0a342d753c78ac83eda8335a9047736717f
craft <yooobuntu@163.com>
2017-02-14 14:55:59 +0800

    scratch: enable linter for models

--- src/models/article.js
+++ src/models/article.js
@@ -1,5 +1,5 @@
 import store from '../store';
-/* eslint-disable */
+
 export default {
   fetchData(API = `${window.location.origin}/assets/articles_01.json`) {
     return new Promise((resolve) => {
@@ -7,12 +7,8 @@ export default {
         return resolve(store.articles);
       }
       return fetch(API)
-        .then((response) => {
-          return ((response.ok) ? response.json() : []);
-        })
-        .then((json) => {
-          resolve(store.articles = json.articles);
-        });
+        .then(response => ((response.ok) ? response.json() : []))
+        .then(json => resolve(store.articles = json.articles));
     });
   },
 };
